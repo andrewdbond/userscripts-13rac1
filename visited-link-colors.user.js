3e5fcda54460d18f9f76138b05290cf66c541e20
Andrew D. Bond <4061227+andrewdbond@users.noreply.github.com>
2020-05-12 09:25:03 +0700

    Update visited-link-colors.user.js

--- visited-link-colors.user.js
+++ visited-link-colors.user.js
@@ -5,21 +5,8 @@
 // @author       13rac1
 // @copyright    2015
 // @license      MIT
-// @include      *
+// @include      https://github.com/*
 // @grant        GM_addStyle
 // ==/UserScript==
 
-switch (location.host) {
-    case "openuserjs.org":
-        GM_addStyle("a:visited {color:#813d0f;}");
-        break;
-    case "github.com":
-        GM_addStyle("a:visited {color:#274a76;}");
-        break;
-    case "bitbucket.org":
-        GM_addStyle("#content a:visited {color:#274a76;}");
-        break;
-    case "www.youtube.com":
-        GM_addStyle("a:visited .title {color:#408 !important;}");
-        break;
-}
+GM_addStyle("a:visited {color:#609;}");
